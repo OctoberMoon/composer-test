e9de8978811cb172a04639b90bf339ab24cf3e72
雾霭 <15516319695@163.com>
2022-02-24 17:56:44 +0800

    feat:add mul div

--- src/Interfaces/CalculatorServiceInterface.php
+++ src/Interfaces/CalculatorServiceInterface.php
@@ -7,4 +7,8 @@ interface CalculatorServiceInterface
     public function add(int $a, int $b): int;
 
     public function minus(int $a, int $b): int;
+
+    public function mul(int $a, int $b): int;
+
+    public function div(int $a, int $b): int;
 }
